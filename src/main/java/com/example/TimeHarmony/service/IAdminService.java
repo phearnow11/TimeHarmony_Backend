8e134562007714c7d0221011c0d5f3f2bd043b75
Phrimp <thaiphienn@gmail.com>
2024-05-15 13:10:06 +0700

    format

--- src/main/java/com/example/TimeHarmony/service/IAdminService.java
+++ src/main/java/com/example/TimeHarmony/service/IAdminService.java
@@ -2,8 +2,13 @@ package com.example.TimeHarmony.service;
 
 import java.util.List;
 
+import com.example.TimeHarmony.entity.Admins;
 import com.example.TimeHarmony.entity.Members;
 
 public interface IAdminService {
+
     public List<Members> getMembers();
+
+    public List<Admins> getAdmins();
+
 }
