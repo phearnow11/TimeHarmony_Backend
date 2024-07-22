4d822b17ef0bd466a8e96ee7e9c3a4e9b8b93218
ThinhPhoenix <laithinh1310@gmail.com>
2024-07-22 08:04:17 +0700

    CORS

--- src/main/java/com/example/TimeHarmony/configuration/WebConfig.java
+++ src/main/java/com/example/TimeHarmony/configuration/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://timeharmony.azurewebsites.net")
+                .allowedOrigins("https://timeharmony.azurewebsites.net","http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
