e41163301d5704df03b7f8030517d865b991b24b
Phrimp <thaiphienn@gmail.com>
2024-07-28 20:49:35 +0700

    list location to one

--- src/main/java/com/example/TimeHarmony/repository/OrderLocationRepository.java
+++ src/main/java/com/example/TimeHarmony/repository/OrderLocationRepository.java
@@ -1,6 +1,7 @@
 package com.example.TimeHarmony.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ import com.example.TimeHarmony.entity.OrderLocation;
 public interface OrderLocationRepository extends JpaRepository<OrderLocation, String> {
     @Query(value = "select * from Order_Location where order_id = :oid", nativeQuery = true)
     List<OrderLocation> getOrderLocations(@Param("oid") String oid);
+
+    @Query(value = "select * from [dbo].[Order_Location] where order_id = :oid", nativeQuery = true)
+    Optional<OrderLocation> getLocationFromOrder(@Param("oid") String oid);
 }
--- src/main/java/com/example/TimeHarmony/service/StaffService.java
+++ src/main/java/com/example/TimeHarmony/service/StaffService.java
@@ -4,6 +4,7 @@ import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,114 +24,124 @@ import jakarta.el.ELException;
 
 @Service
 public class StaffService implements IStaffService {
-    @Autowired
-    private WatchRepository WATCH_REPOSITORY;
-    @Autowired
-    private OrderRepository ORDER_REPOSITORY;
-    @Autowired
-    private StaffRepository STAFF_REPOSITORY;
-    @Autowired
-    private OrderLocationRepository ORDER_LOCATION_REPOSITORY;
-    @Autowired
-    private StringService STRING_SERVICE;
+  @Autowired
+  private WatchRepository WATCH_REPOSITORY;
+  @Autowired
+  private OrderRepository ORDER_REPOSITORY;
+  @Autowired
+  private StaffRepository STAFF_REPOSITORY;
+  @Autowired
+  private OrderLocationRepository ORDER_LOCATION_REPOSITORY;
+  @Autowired
+  private StringService STRING_SERVICE;
 
-    @Override
-    public String approveWatch(String watch_id) {
-        byte APPROVED_STATE = 1;
-        try {
-            WATCH_REPOSITORY.approveWatch(Timestamp.valueOf(LocalDateTime.now()), watch_id, APPROVED_STATE);
-            ;
-            return "Watch Approved";
-        } catch (Exception e) {
-            return e.toString();
-        }
+  @Override
+  public String approveWatch(String watch_id) {
+    byte APPROVED_STATE = 1;
+    try {
+      WATCH_REPOSITORY.approveWatch(Timestamp.valueOf(LocalDateTime.now()), watch_id, APPROVED_STATE);
+      ;
+      return "Watch Approved";
+    } catch (Exception e) {
+      return e.toString();
+    }
+  }
+
+  @Override
+  public List<Watch> getAllStateWatch(int state) {
+    try {
+      return WATCH_REPOSITORY.getWatchesByState(state);
+    } catch (Exception e) {
+      return null;
     }
 
-    @Override
-    public List<Watch> getAllStateWatch(int state) {
-        try {
-            return WATCH_REPOSITORY.getWatchesByState(state);
-        } catch (Exception e) {
-            return null;
-        }
+  }
 
+  @Override
+  public String unApproveWatch(String watch_id) {
+    byte UNAPPROVED_STATE = 2;
+    try {
+      WATCH_REPOSITORY.approveWatch(Timestamp.valueOf(LocalDateTime.now()), watch_id, UNAPPROVED_STATE);
+      ;
+      return "Watch Deleted";
+    } catch (Exception e) {
+      return e.toString();
     }
+  }
 
-    @Override
-    public String unApproveWatch(String watch_id) {
-        byte UNAPPROVED_STATE = 2;
-        try {
-            WATCH_REPOSITORY.approveWatch(Timestamp.valueOf(LocalDateTime.now()), watch_id, UNAPPROVED_STATE);
-            ;
-            return "Watch Deleted";
-        } catch (Exception e) {
-            return e.toString();
-        }
+  @Override
+  public String shipOrder(String id, String oid) {
+    try {
+      if (STAFF_REPOSITORY.getRole(UUID.fromString(id)) != StaffRole.SHIPPER)
+        throw new Exception("You are not shipper");
+      if (ORDER_REPOSITORY.getOrderWatchStates(oid).contains(3))
+        throw new Exception("Order is not packed");
+      if (ORDER_REPOSITORY.getState(oid) == OrderState.SHIPPING)
+        throw new Exception("Order is already shipping");
+      ORDER_REPOSITORY.shipOrder(oid);
+      STAFF_REPOSITORY.shippingOrder(UUID.fromString(id), oid);
+      return "Order is shipping";
+    } catch (Exception e) {
+      return e.toString();
     }
+  }
 
-    @Override
-    public String shipOrder(String id, String oid) {
-        try {
-            if (STAFF_REPOSITORY.getRole(UUID.fromString(id)) != StaffRole.SHIPPER)
-                throw new Exception("You are not shipper");
-            if (ORDER_REPOSITORY.getOrderWatchStates(oid).contains(3))
-                throw new Exception("Order is not packed");
-            if (ORDER_REPOSITORY.getState(oid) == OrderState.SHIPPING)
-                throw new Exception("Order is already shipping");
-            ORDER_REPOSITORY.shipOrder(oid);
-            STAFF_REPOSITORY.shippingOrder(UUID.fromString(id), oid);
-            return "Order is shipping";
-        } catch (Exception e) {
-            return e.toString();
-        }
+  @Override
+  public List<String> getMyShippingOrder(String id) {
+    return ORDER_REPOSITORY.getShippingOrderFromShipper(UUID.fromString(id));
+  }
+
+  @Override
+  public String shippedOrder(String id, String oid) {
+    try {
+      if (STAFF_REPOSITORY.getRole(UUID.fromString(id)) != StaffRole.SHIPPER)
+        throw new Exception("You are not shipper");
+      if (ORDER_REPOSITORY.getState(oid) != OrderState.SHIPPING)
+        throw new Exception("Logic error");
+      ORDER_REPOSITORY.shippedOrder(oid);
+      STAFF_REPOSITORY.shippedOrder(oid);
+      WATCH_REPOSITORY.shippedOrder(oid);
+      return "Order shipped confirm";
+    } catch (Exception e) {
+      return e.toString();
     }
+  }
 
-    @Override
-    public List<String> getMyShippingOrder(String id) {
-        return ORDER_REPOSITORY.getShippingOrderFromShipper(UUID.fromString(id));
+  @Override
+  public String updateLocation(String id, Map<String, Object> data) {
+    try {
+
+      if (data.get("oid") == null)
+        throw new ELException("Missing Order ID");
+
+      String oid = data.get("oid").toString();
+      OrderState state = ORDER_REPOSITORY.getState(oid);
+
+      if (state == OrderState.DELETED || state == OrderState.PENDING || state == OrderState.FAILED)
+        throw new Exception("Logic Error");
+
+      Optional<OrderLocation> cur_Location = ORDER_LOCATION_REPOSITORY.getLocationFromOrder(oid);
+      Double latitude = (Double) data.get("latitude");
+      Double longtitude = (Double) data.get("longtitude");
+
+      if (cur_Location.isEmpty()) {
+        String lid = "L" + STRING_SERVICE.autoGenerateString(11);
+        OrderLocation new_Location = new OrderLocation(lid, oid, latitude, longtitude, UUID.fromString(id),
+            Timestamp.valueOf(LocalDateTime.now()), null,
+            data.get("note") == null ? null : data.get("note").toString());
+        ORDER_LOCATION_REPOSITORY.save(new_Location);
+      } else {
+        OrderLocation new_Location = cur_Location.get();
+        new_Location.setLatitude(latitude);
+        new_Location.setLongitude(longtitude);
+        new_Location.setUpdated_at(Timestamp.valueOf(LocalDateTime.now()));
+        ORDER_LOCATION_REPOSITORY.save(new_Location);
+      }
+
+      return "Location updated at " + LocalDateTime.now();
+    } catch (Exception e) {
+      return e.toString();
     }
-
-    @Override
-    public String shippedOrder(String id, String oid) {
-        try {
-            if (STAFF_REPOSITORY.getRole(UUID.fromString(id)) != StaffRole.SHIPPER)
-                throw new Exception("You are not shipper");
-            if (ORDER_REPOSITORY.getState(oid) != OrderState.SHIPPING)
-                throw new Exception("Logic error");
-            ORDER_REPOSITORY.shippedOrder(oid);
-            STAFF_REPOSITORY.shippedOrder(oid);
-            WATCH_REPOSITORY.shippedOrder(oid);
-            return "Order shipped confirm";
-        } catch (Exception e) {
-            return e.toString();
-        }
-    }
-
-    @Override
-    public String updateLocation(String id, Map<String, Object> data) {
-        try {
-
-            if (data.get("oid") == null)
-                throw new ELException("Missing Order ID");
-
-            String oid = data.get("oid").toString();
-            OrderState state = ORDER_REPOSITORY.getState(oid);
-
-            if (state == OrderState.DELETED || state == OrderState.PENDING || state == OrderState.FAILED)
-                throw new Exception("Logic Error");
-
-            String lid = "L" + STRING_SERVICE.autoGenerateString(11);
-            Double latitude = (Double) data.get("latitude");
-            Double longtitude = (Double) data.get("longtitude");
-
-            ORDER_LOCATION_REPOSITORY.save(new OrderLocation(lid, oid, latitude, longtitude,
-                    UUID.fromString(id), Timestamp.valueOf(LocalDateTime.now()),
-                    data.get("note") == null ? null : data.get("note").toString()));
-
-            return "Location updated at " + LocalDateTime.now();
-        } catch (Exception e) {
-            return e.toString();
-        }
-    }
+  }
 
 }
