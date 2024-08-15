56878b1cf88c841147f8aacb834fd7a12d7974c4
Phrimp <phien.nt@thdcybersecurity.xyz>
2024-08-15 09:16:28 +0700

    fix get my shipping order

--- src/main/java/com/example/TimeHarmony/repository/OrderRepository.java
+++ src/main/java/com/example/TimeHarmony/repository/OrderRepository.java
@@ -67,7 +67,7 @@ public interface OrderRepository extends JpaRepository<Orders, String> {
   @Query(value = "select * from [dbo].[Orders] where state = :state ", nativeQuery = true)
   List<Orders> getOrderByState(@Param("state") int state);
 
-  @Query(value = "select [dbo].[Orders].order_id from [dbo].[Orders] join [dbo].[Shipping_Order] on [dbo].[Orders].order_id = [dbo].[Shipping_Order].order_id where shipper = :shipper", nativeQuery = true)
+  @Query(value = "select [dbo].[Orders].order_id from [dbo].[Orders] join [dbo].[Shipping_Order] on [dbo].[Orders].order_id = [dbo].[Shipping_Order].order_id where shipper = :shipper and [dbo].[Shipping_Order].status = 1", nativeQuery = true)
   List<String> getShippingOrderFromShipper(@Param("shipper") UUID shipper);
 
   @Query(value = "select [dbo].[Orders].order_id from [dbo].[Orders] join [dbo].[Shipping_Order] on [dbo].[Orders].order_id = [dbo].[Shipping_Order].order_id", nativeQuery = true)
